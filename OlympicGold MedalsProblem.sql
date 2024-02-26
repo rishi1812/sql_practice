189209bff72ab79a676c7c78e597aeb23212a3ea
rishi1812 <43420125+rishi1812@users.noreply.github.com>
2024-02-27 00:40:04 +0530

    Update OlympicGold MedalsProblem.sql

--- OlympicGold MedalsProblem.sql
+++ OlympicGold MedalsProblem.sql
@@ -13,3 +13,13 @@ select player_name from cte cte1 where not exists
 (select player_name from cte cte2 where cte1.player_name=cte2.player_name and cte2.award!='gold' )
 and cte1.award='gold')
 select player_name,count(*) from cte2 group by player_name
+--using join
+with cte as (
+select g_events.gold player_name,s_events.silver,b_events.bronze from events g_events
+left join events s_events on g_events.gold=s_events.silver
+left join events b_events on g_events.gold=b_events.bronze	
+where s_events.silver is null and b_events.bronze is null
+)
+select player_name,count(*)
+from cte
+group by player_name
